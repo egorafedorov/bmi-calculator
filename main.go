0dd4aed61aec52eb23d9f4788cb6f0114de9408a
egorafedorov <egorafedorovme@gmail.com>
2025-06-30 17:34:47 +0300

    Iteration 01

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+	"github.com/fatih/color"
+)
+
+const BMIPower = 2
 
 func main() {
-	fmt.Print("Start")
+	fmt.Println("*** BMI Calculator ***")
+	userWeight, userHeight := getUserInput()
+	BMI := calculateBMI(userWeight, userHeight)
+	outputResult(BMI)
+
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("Enter your height in centimeters: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Enter your weight in kilograms: ")
+	fmt.Scan(&userWeight)
+	return userWeight, userHeight
+}
+
+func calculateBMI(userWeight float64, userHeight float64) float64 {
+	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
+	return BMI
+}
+
+func outputResult(BMI float64) {
+	result := fmt.Sprintf("Your body mass index: %.0f", BMI)
+	color.Blue(result)
 }
