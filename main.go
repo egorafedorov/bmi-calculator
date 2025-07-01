5c495a143fb93cfa9e90066db4872892da6396dc
egorafedorov <egorafedorovme@gmail.com>
2025-07-01 10:08:40 +0300

    Iteration 02

--- main.go
+++ main.go
@@ -11,10 +11,15 @@ const BMIPower = 2
 
 func main() {
 	fmt.Println("*** BMI Calculator ***")
-	userWeight, userHeight := getUserInput()
-	BMI := calculateBMI(userWeight, userHeight)
-	outputResult(BMI)
-
+	for {
+		userWeight, userHeight := getUserInput()
+		BMI := calculateBMI(userWeight, userHeight)
+		outputResult(BMI)
+		userRepeatCalculation := userRepeatCalculation()
+		if !userRepeatCalculation {
+			break
+		}
+	}
 }
 
 func getUserInput() (float64, float64) {
@@ -35,4 +40,27 @@ func calculateBMI(userWeight float64, userHeight float64) float64 {
 func outputResult(BMI float64) {
 	result := fmt.Sprintf("Your body mass index: %.0f", BMI)
 	color.Blue(result)
+	switch true {
+	case BMI < 16:
+		color.Red("You are severely underweight")
+	case BMI < 18.5:
+		color.Yellow("You are underweight")
+	case BMI < 25:
+		color.Green("You are of normal weight")
+	case BMI < 30:
+		color.Yellow("You are overweight")
+	default:
+		color.Red("You have a degree of obesity")
+	}
+}
+
+func userRepeatCalculation() bool {
+	var userChoice string
+	fmt.Print("Do you want to repeat the calculation? (Y/N): ")
+	fmt.Scan(&userChoice)
+	if userChoice == "Y" || userChoice == "y" {
+		return true
+	} else {
+		return false
+	}
 }
